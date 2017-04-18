506c46e60cf54946ccf6211aa63d5bbf02158a90
alihk12@mail.com <alihk12@mail.com>
2017-04-18 16:53:33 +0430

    fix authentication page

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'locallibrary.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['../templates', ],
+        'DIRS': ['./locallibrary/templates', ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
